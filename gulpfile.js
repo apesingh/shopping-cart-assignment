88bbb0c003a762a261f6b1415560581e0f225de4
Apeksha Singh <apeksha.singh@publicissapient.com>
2020-01-16 20:22:19 +0530

    code commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
+concatCss = require('gulp-concat-css'),
 sass = require('gulp-sass');
 gulp.task('styles', function(){
-return gulp.src(['style/*.scss'])
-.pipe(sass()) // Converts Sass to CSS with gulp-sass
-      .pipe(gulp.dest('style'))
+return gulp.src([
+    'style/*.scss'
+])
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('style'))
 });
 
 //Default task array
 gulp.task("default", gulp.series('styles')); 
 
+//Auto prefix
 const autoprefixer = require('gulp-autoprefixer');
-
 gulp.task('prefix', () =>
-    gulp.src(['style/*.css'])
+    gulp.src([
+        'style/*.css'
+    ])
         .pipe(autoprefixer({
             browsers: ['last 99 versions'],
             cascade: false
     }))
+    .pipe(gulp.dest('style'))
+);
+
+//Create bundle of all css files
+gulp.task('concatCss', () =>
+    gulp.src([
+        'style/*.css',
+        '!style/reset.css' // <== ignore this file for concat
+    ])
+    .pipe(concatCss("bundle.css"))
     .pipe(gulp.dest('style'))
 );
  
